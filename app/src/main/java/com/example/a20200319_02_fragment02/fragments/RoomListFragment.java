9617a74b5719a31af9725b244fe991b6c5cf9861
seakjh <skykjh0513@gmail.com>
2020-03-19 15:24:44 +0900

    방목록 3개 추가

--- app/src/main/java/com/example/a20200319_02_fragment02/fragments/RoomListFragment.java
+++ app/src/main/java/com/example/a20200319_02_fragment02/fragments/RoomListFragment.java
@@ -57,6 +57,8 @@ public class RoomListFragment extends Fragment {
 //        목록이 방을 여러개 추가하고, 어댑터 새로고침
 
         roomList.add(new Room(5000, "서울시 은평구"));
+        roomList.add(new Room(8000, "서울시 서대문구"));
+        roomList.add(new Room(12000, "서울시 종로구"));
 
 //        어댑터에게 내용 변경이 있었다고 새로고침
         adapter.notifyDataSetChanged();
