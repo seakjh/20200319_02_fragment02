1e2b541a366089f9e1fc38577ab5d4c8fcc101da
seakjh <skykjh0513@gmail.com>
2020-03-19 12:23:30 +0900

    프로젝트 기본 세팅

---
+++ app/src/main/java/com/example/a20200319_02_fragment02/BaseActivity.java
@@ -0,0 +1,15 @@
+package com.example.a20200319_02_fragment02;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setupEvents();
+
+    public abstract void setValues();
+
+}
