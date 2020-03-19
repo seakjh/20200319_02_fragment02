6e886d30e5d72ad1f0c52334b0b51fe5aca0fe1d
seakjh <skykjh0513@gmail.com>
2020-03-19 12:59:58 +0900

    앱의 시작화면을 splash액티비티로 변경

--- app/src/main/java/com/example/a20200319_02_fragment02/SplashActivity.java
+++ app/src/main/java/com/example/a20200319_02_fragment02/SplashActivity.java
@@ -1,15 +1,20 @@
 package com.example.a20200319_02_fragment02;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 
+import com.example.a20200319_02_fragment02.databinding.ActivitySplashBinding;
+
 public class SplashActivity extends BaseActivity {
+
+    ActivitySplashBinding binding = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash);
     }
 
     @Override
